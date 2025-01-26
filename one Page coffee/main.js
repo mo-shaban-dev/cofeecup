092ffb1f8905d9062a590cbc2c3018dd96e51702
‪Mohamed Shabab‬n <dev.m.shaban@gmail.com>
2025-01-26 09:04:21 +0200

    Delete one Page coffee directory

--- one Page coffee/main.js
+++
@@ -1,7 +0,0 @@
-const ordertabe = document.getElementById("order");
-const form = document.getElementById("form");
-
-
-ordertabe.addEventListener("click", () => {
-    form.style.display = "block";
-});
